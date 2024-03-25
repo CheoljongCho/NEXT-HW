95baab52fcf495bd3052ef6ce502a6fbb8969e00
CheoljongCho <cjjong0512@gmail.com>
2024-03-25 17:55:45 +0900

    과제 최종

--- NEXT_HW_6/wantedcrawling.py
+++ NEXT_HW_6/wantedcrawling.py
@@ -5,9 +5,20 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from openpyxl import Workbook
 
+# 엑셀 파일 경로
+file_path = 'C:/Users/Cheoljong/Desktop/NEXT/HW/NEXT_HW_6/wanted_jobpost_dev.xlsx'
+
+# 엑셀 워크북 생성
+wb = Workbook()
+
+# 엑셀 시트 선택
+ws = wb.active
+
+# 데이터 생성
+header = ["번호", "제목", "회사"]
+ws.append(header)
 chromedriver_path = "C:/Users/Cheoljong/Desktop/NEXT/Session6/chromedriver.exe"
 
 user_data_dir = "C:/Users/Cheoljong/Desktop/NEXT/HW/NEXT_HW_6/cash"
@@ -39,15 +50,16 @@ while f5 < 8:
   time.sleep(2)
   f5 += 1
 
-file = open('wantedjobpostdev', mode="w", newline='')
-writer = csv.writer(file)
-writer.writerow(["num", "name","company"])
 
 for i in range(1, 101):
-    name = driver.find_element(By.XPATH, f'//*[@id="__next"]/div[3]/div[2]/ul/li[{i}]/div/a/div[2]/p').text
-    company = driver.find_element(By.XPATH, f'//*[@id="__next"]/div[3]/div[2]/ul/li[{i}]/div/a/div[2]/span').text
+    names = driver.find_element(By.XPATH, f'//*[@id="__next"]/div[3]/div[2]/ul/li[{i}]/div/a/div[2]/p').text
+    companies = driver.find_element(By.XPATH, f'//*[@id="__next"]/div[3]/div[2]/ul/li[{i}]/div/a/div[2]/span').text
     num = i
    
-    print(num, name, company)
+    print(num, names, companies)
+    
+    ws.append([num, names, companies])
 
-file.close()
+wb.save(file_path)
+
+print("엑셀 파일 저장 완료")
