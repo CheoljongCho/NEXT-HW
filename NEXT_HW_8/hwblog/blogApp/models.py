5ba9ee58c218bd1f741ac673e001d9f98872d663
CheoljongCho <cjjong0512@gmail.com>
2024-04-04 17:42:15 +0900

    css 만지는중

--- NEXT_HW_8/hwblog/blogApp/models.py
+++ NEXT_HW_8/hwblog/blogApp/models.py
@@ -5,10 +5,9 @@ class Article(models.Model):
   title = models.CharField(max_length=200)
   content = models.TextField()
   CATEGORY_CHOICES = [
-    ('Habit', '취미'),
+    ('Hobby', '취미'),
     ('Food', '음식'),
     ('Programming', '프로그래밍'),
-    ('none', '기타'),
   ]
   category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
   reg_time = models.DateTimeField(auto_now_add=True)
